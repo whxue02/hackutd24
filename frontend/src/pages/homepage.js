7486149f01df875e2987e18b6bd2634517d47b34
Cheryl Wang <whxcollege@gmail.com>
2024-11-16 16:27:18 -0600

    worked on landing page

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,12 +1,33 @@
 import Navbar from "../components/navbar"
+import { Link } from "react-router-dom"
 import "./cheryl.css"
 
 const Homepage = () => {
     return(
-        <div>
+        <div className="homepageBody">
             <Navbar/>
-            <div className="homepageBody">
-
+            <div className="containter left">
+                <div>
+                    <img src="/images/toyota.png" className="title"></img>
+                </div>
+                <div>
+                    <p className="subTitle">EcoVision</p>
+                </div>
+                <div>
+                    <p className="text">Clear Insights, Efficient Futures.</p>
+                </div>
+                <div>
+                    <Link to="/analyze">
+                    <button className="analyze">
+                        Analyze
+                    </button>
+                    </Link>
+                    <Link to="/upload">
+                    <button>
+                        Upload
+                    </button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
